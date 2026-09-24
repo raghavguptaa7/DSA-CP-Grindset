5b557bfe78a4ead6224410e9cf34a81f39f4e854
Raghav Gupta <guptaraghav2277@gmail.com>
2026-09-24 11:46:26 +0530

    Added leetocde 3550  solution

---
+++ Leetcode/Easy/2996.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        int sum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]+1){
+                sum+=nums[i];
+            }else{
+                break;
+            }
+        }
+        HashSet<Integer>seen= new HashSet<>();
+        for(int num:nums){
+            seen.add(num);
+        }
+        int ans= sum;
+        while(seen.contains(ans)){
+            ans++;
+        }
+        return ans;
+    }
+}
